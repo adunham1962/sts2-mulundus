a98c2c25dc79d6a74f43496ab20b4af644ef672d
Austin Dunham <afdunham1962@gmail.com>
2026-08-02 19:11:23 -0400

    :tada: New build!
    ## Added
    * :sparkles: Flowing Flurry
      * A new Common Attack for the Emerald Monk
      * It returns to your hand whenever your hand whenever you play another card with Flow!

---
+++ STS2_MulundusCode/Cards/EmeraldMonk/Common/FlowingFlurry.cs
@@ -0,0 +1,37 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using STS2_Mulundus.STS2_MulundusCode.Character;
+
+namespace STS2_Mulundus.STS2_MulundusCode.Cards.EmeraldMonk.Common;
+
+[Pool(typeof(EmeraldMonkCardPool))]
+public class FlowingFlurry : EmeraldMonkCard
+{
+    public FlowingFlurry() : base(0, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+        WithDamage(4);
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
+    {
+        var card = cardPlay.Card;
+        if (card.Owner == Owner && card != this && card is EmeraldMonkCard && card.HasFlow() && CardPile.GetCards(Owner, PileType.Discard).Contains(this))
+        {
+            await CardPileCmd.Add(this, PileType.Hand);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
